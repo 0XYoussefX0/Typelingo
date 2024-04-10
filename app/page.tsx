27b5dd4f94bab432a71beea72bdb4ab2300ec808
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-04-10 12:40:14 +0000

    completed the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "@/app/_assets/logo.svg";
+import planet_icon from "@/app/_assets/planet_icon.svg";
 
 import { Button } from "@/components/button";
 
@@ -9,8 +10,21 @@ export default function Home() {
       <nav className="flex justify-center items-center p-4">
         <Image src={logo} alt="typelingo's logo" />
       </nav>
-      <main className="flex-1 bg-[url('@/app/_assets/starsBg.png')]">
-        <Button>Get Started</Button>
+      <main className="flex-1 bg-[url('@/app/_assets/starsBg.png')] flex items-center justify-center gap-[140px]">
+        <div>
+          <Image src={planet_icon} alt="" />
+        </div>
+        <div className="flex items-center flex-col gap-12">
+          <h1 className="text-white font-bold text-center text-[32px] ">
+            The free, fun, and effective way to
+            <br />
+            learn a language!
+          </h1>
+          <div className="flex flex-col gap-3">
+            <Button>GET STARTED</Button>
+            <Button variant={"secondary"}>I ALREADY HAVE AN ACCOUNT</Button>
+          </div>
+        </div>
       </main>
     </div>
   );
