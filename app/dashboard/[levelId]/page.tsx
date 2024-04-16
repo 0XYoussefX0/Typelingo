c0a9f8f10f68b6d56e8ed286240243280b7bad1c
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-04-16 10:53:00 +0100

    add function that checks the user's solution using typescript compiler api

--- app/dashboard/[levelId]/page.tsx
+++ app/dashboard/[levelId]/page.tsx
@@ -1,82 +1,134 @@
 "use client";
 
+import { useRef } from "react";
+
 import ProgressBar from "@/components/ui/progressBar";
 import { Button } from "@/components/ui/button";
 
 import closeIcon from "@/app/_assets/closeIcon.svg";
 import Image from "next/image";
 
-import Editor from "@monaco-editor/react";
+import Editor, { Monaco } from "@monaco-editor/react";
+import { useRouter } from "next/navigation";
+
+import { createProject, ts } from "@ts-morph/bootstrap";
+
+import * as monaco from "monaco-editor";
+
+import { unstable_noStore } from "next/cache";
 
 function Page() {
-  const value = `
-/* _____________ Your Code Here _____________ */
+  unstable_noStore();
 
-type Flatten<S extends any[], T extends any[] = []> =  S extends [infer X, ...infer Y] ? 
-  X extends any[] ?
-   Flatten<[...X, ...Y], T> : Flatten<[...Y], [...T, X]> 
-  : T
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
 
-type cases = [
-  Expect<Equal<Flatten<[]>, []>>,
-  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
-  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-  Expect<Equal<Flatten<[{ foo: 'bar', 2: 10 }, 'foobar']>, [{ foo: 'bar', 2: 10 }, 'foobar']>>,
-]
+  /* the value of this variable is going to be coming from the challenges database */
+  const value = `  
 
-// @ts-expect-error
-type error = Flatten<'1'>
+  /* _____________ Your Code Here _____________ */
 
-`;
+  type Flatten<S extends any[], T extends any[] = []> =  S extends [infer X, ...infer Y] ? 
+    X extends any[] ?
+     Flatten<[...X, ...Y], T> : Flatten<[...Y], [...T, X]> 
+    : T
+  
+  type cases = [
+    Expect<Equal<Flatten<[]>, []>>,
+    Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+    Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+    Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+    Expect<Equal<Flatten<[{ foo: 'bar', 2: 10 }, 'foobar']>, [{ foo: 'bar', 2: 10 }, 'foobar']>>,
+  ]
+  
+  // @ts-expect-error
+  type error = Flatten<'1'>
+  
+  `;
 
-  function handleEditorWillMount(monaco: any) {
+  const typesForTesting = `
+  type Expect<T extends true> = T;
+  type ExpectTrue<T extends true> = T;
+  type ExpectFalse<T extends false> = T;
+  type IsTrue<T extends true> = T;
+  type IsFalse<T extends false> = T;
+ 
+  type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+   T,
+ >() => T extends Y ? 1 : 2
+   ? true
+   : false;
+  type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
+ 
+ // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+  type IsAny<T> = 0 extends 1 & T ? true : false;
+  type NotAny<T> = true extends IsAny<T> ? false : true;
+ 
+  type Debug<T> = { [K in keyof T]: T[K] };
+  type MergeInsertions<T> = T extends object
+   ? { [K in keyof T]: MergeInsertions<T[K]> }
+   : T;
+ 
+  type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
+ 
+  type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE
+   ? true
+   : false;
+  type ExpectValidArgs<
+   FUNC extends (...args: any[]) => any,
+   ARGS extends any[],
+ > = ARGS extends Parameters<FUNC> ? true : false;
+ 
+  type UnionToIntersection<U> = (
+   U extends any ? (k: U) => void : never
+ ) extends (k: infer I) => void
+   ? I
+   : never;
+ 
+  const doNotExecute = (func: () => any) => {};`;
+
+  function handleEditorWillMount(monaco: Monaco) {
     // here is the monaco instance
     // do something before editor is mounted
     monaco.languages.typescript.typescriptDefaults.addExtraLib(
-      `
-     type Expect<T extends true> = T;
-     type ExpectTrue<T extends true> = T;
-     type ExpectFalse<T extends false> = T;
-     type IsTrue<T extends true> = T;
-     type IsFalse<T extends false> = T;
-    
-     type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-      T,
-    >() => T extends Y ? 1 : 2
-      ? true
-      : false;
-     type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
-    
-    // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
-     type IsAny<T> = 0 extends 1 & T ? true : false;
-     type NotAny<T> = true extends IsAny<T> ? false : true;
-    
-     type Debug<T> = { [K in keyof T]: T[K] };
-     type MergeInsertions<T> = T extends object
-      ? { [K in keyof T]: MergeInsertions<T[K]> }
-      : T;
-    
-     type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
-    
-     type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE
-      ? true
-      : false;
-     type ExpectValidArgs<
-      FUNC extends (...args: any[]) => any,
-      ARGS extends any[],
-    > = ARGS extends Parameters<FUNC> ? true : false;
-    
-     type UnionToIntersection<U> = (
-      U extends any ? (k: U) => void : never
-    ) extends (k: infer I) => void
-      ? I
-      : never;
-    
-     const doNotExecute = (func: () => any) => {};`,
+      typesForTesting,
       "file:///node_modules/@types/my-lib/index.d.ts"
     );
   }
+
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: Monaco
+  ) => {
+    editorRef.current = editor;
+  };
+
+  const router = useRouter();
+  const currentLevel = 1; /* change the value of this variable with the current page param */
+
+  const skip = () => {
+    // update the completed column in the database
+    router.push(`/dashboard/${currentLevel + 1}`);
+  };
+
+  const check = async () => {
+    const project = await createProject({ useInMemoryFileSystem: true });
+
+    if (editorRef.current === undefined) return;
+
+    const userCode = editorRef.current.getValue();
+
+    const finalCode = typesForTesting + userCode;
+    console.log(finalCode);
+
+    project.fileSystem.writeFileSync("MyClass.ts", finalCode);
+
+    const program = project.createProgram({
+      rootNames: ["MyClass.ts"],
+      options: {},
+    });
+
+    const diagnostics = program.getSemanticDiagnostics();
+  };
 
   return (
     <div className="flex flex-col w-full h-screen">
@@ -104,14 +156,21 @@ type error = Flatten<'1'>
               defaultLanguage="typescript"
               defaultValue={value}
               beforeMount={handleEditorWillMount}
+              onMount={handleEditorDidMount}
             />
           </div>
         </div>
         <div className="h-[140px] border-t-2 border-light-grey border-solid flex justify-between w-full items-center px-[10%]">
-          <Button variant={"secondary"} className="w-[149px]" on>
+          <Button
+            variant={"secondary"}
+            className="w-[149px]"
+            onClick={() => skip()}
+          >
             SKIP
           </Button>
-          <Button className="w-[159px]">CHECK</Button>
+          <Button className="w-[159px]" onClick={() => check()}>
+            CHECK
+          </Button>
         </div>
       </main>
     </div>
