f55f01e9c19e53d874621789832ff2399c6081b6
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-04-20 13:27:51 +0100

    Refactor code editor component and remove unused code

--- components/challenge.tsx
+++ components/challenge.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState, useRef } from "react";
-
-import { Editor, Monaco } from "@monaco-editor/react";
+import { useRef } from "react";
 
 import { editor } from "monaco-editor";
 import { challengeStatusStore } from "./challengeBanner";
+
+import CodeEdtior from "./ui/codeEditor";
 
 type ChallengeProps = {
   challengeCode: string;
@@ -23,79 +23,6 @@ function Challenge({
     (state) => state.challengeStatus
   );
 
-  const typesForTesting = `
-  type Expect<T extends true> = T;
-  type ExpectTrue<T extends true> = T;
-  type ExpectFalse<T extends false> = T;
-  type IsTrue<T extends true> = T;
-  type IsFalse<T extends false> = T;
- 
-  type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-   T,
- >() => T extends Y ? 1 : 2
-   ? true
-   : false;
-  type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
- 
- // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
-  type IsAny<T> = 0 extends 1 & T ? true : false;
-  type NotAny<T> = true extends IsAny<T> ? false : true;
- 
-  type Debug<T> = { [K in keyof T]: T[K] };
-  type MergeInsertions<T> = T extends object
-   ? { [K in keyof T]: MergeInsertions<T[K]> }
-   : T;
- 
-  type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
- 
-  type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE
-   ? true
-   : false;
-  type ExpectValidArgs<
-   FUNC extends (...args: any[]) => any,
-   ARGS extends any[],
- > = ARGS extends Parameters<FUNC> ? true : false;
- 
-  type UnionToIntersection<U> = (
-   U extends any ? (k: U) => void : never
- ) extends (k: infer I) => void
-   ? I
-   : never;
- 
-  const doNotExecute = (func: () => any) => {};`;
-
-  const value = `  
-
-  /* _____________ Your Code Here _____________ */
-
-  type Flatten<S extends any[], T extends any[] = []> =  S extends [infer X, ...infer Y] ? 
-    X extends any[] ?
-     Flatten<[...X, ...Y], T> : Flatten<[...Y], [...T, X]> 
-    : T
-  
-  type cases = [
-    Expect<Equal<Flatten<[]>, []>>,
-    Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
-    Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
-    Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-    Expect<Equal<Flatten<[{ foo: 'bar', 2: 10 }, 'foobar']>, [{ foo: 'bar', 2: 10 }, 'foobar']>>,
-  ]
-  
-  // @ts-expect-error
-  type error = Flatten<'1'>
-  
-  `;
-
-  function handleEditorWillMount(monaco: Monaco) {
-    monaco.languages.typescript.typescriptDefaults.addExtraLib(
-      typesForTesting,
-      "file:///node_modules/@types/my-lib/index.d.ts"
-    );
-  }
-
-  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
-    editorRef.current = editor;
-  };
   return (
     <main
       className={`flex flex-col gap-20 items-center justify-between mt-10 flex-1 ${
@@ -112,13 +39,7 @@ function Challenge({
           </p>
         </div>
         <div className="rounded-2xl border-2 border-solid border-light-grey w-[80%] h-fit">
-          <Editor
-            height="40vh" // modify the height depending on the viewport or make it the remaining height in the screen and add some margins between other elements
-            defaultLanguage="typescript"
-            defaultValue={challengeCode}
-            beforeMount={handleEditorWillMount}
-            onMount={handleEditorDidMount}
-          />
+          <CodeEdtior challengeCode={challengeCode} />
         </div>
       </div>
     </main>
--- components/ui/codeEditor.tsx
+++ components/ui/codeEditor.tsx
@@ -1,16 +1,64 @@
-import React from "react";
+"use client";
 
-function CodeEdtior() {
-  // implement some
+import { Editor, Monaco } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+
+import { typesForTesting } from "@/lib/types";
+
+import { create } from "zustand";
+
+import { challengeStatusStore } from "../challengeBanner";
+
+type EditorRefStore = {
+  editorRef: editor.IStandaloneCodeEditor | null;
+  setEditorRef: (editor: editor.IStandaloneCodeEditor) => void;
+};
+export const editorRefStore = create<EditorRefStore>((set) => ({
+  editorRef: null,
+  setEditorRef: (editor) => set({ editorRef: editor }),
+}));
+
+type CodeEditorProps = {
+  challengeCode: string;
+};
+
+function CodeEditor({ challengeCode }: CodeEditorProps) {
+  const challengeStatus = challengeStatusStore(
+    (state) => state.challengeStatus
+  );
+  const setChallengeStatus = challengeStatusStore(
+    (state) => state.setChallengeStatus
+  );
+
+  const setEditorRef = editorRefStore((state) => state.setEditorRef);
+
+  function handleEditorWillMount(monaco: Monaco) {
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(
+      typesForTesting,
+      "file:///node_modules/@types/my-lib/index.d.ts"
+    );
+  }
+
+  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
+    setEditorRef(editor);
+  };
+
+  const handleEditorChange = () => {
+    if (challengeStatus === "failed") {
+      setChallengeStatus(undefined);
+    }
+  };
+
   return (
     <Editor
+      onChange={handleEditorChange}
       height="40vh"
       defaultLanguage="typescript"
-      defaultValue={value}
+      defaultValue={challengeCode}
       beforeMount={handleEditorWillMount}
       onMount={handleEditorDidMount}
     />
   );
 }
 
-export default CodeEdtior;
+export default CodeEditor;
